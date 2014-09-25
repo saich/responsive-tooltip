6faeabae086af1c2dec3229ec2246bab790f4a95
Sai Prasad <saiprasad.ch@gmail.com>
2014-09-25 16:01:11 +0530

    A simplistic implementation to minify JS file into dist/ using uglify.

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,11 +11,28 @@ module.exports = function (grunt) {
         		jshintrc: true,
         		extensions: 'json'
         	}
+        },
+        uglify: {
+            options: {
+                mangle: true,
+                compress: true,
+                sourceMap: true,
+                preserveComments: 'some'
+            },
+            all: {
+                options: {
+                    sourceMapName: 'dist/tooltip.min.map'
+                },
+                files: {
+                    'dist/tooltip.min.js': ['tooltip.js']
+                }
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
 
     // Default task(s).
-	grunt.registerTask('default', ['jshint']);
+	grunt.registerTask('default', ['jshint', 'uglify']);
 };
